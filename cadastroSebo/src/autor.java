3a5bfaab533c556a0724a2e08d352baf8e7a301d
Anderson Bertolo <21359810870@info.local>
2023-10-11 21:55:57 -0300

    exemplo cadastro livro

---
+++ cadastroSebo/src/autor.java
@@ -0,0 +1,7 @@
+public class autor
+    {
+    public String nome;
+    public String localNasc;
+    }
+
+
---
+++ cadastroSebo/src/livro.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+
+public class livro {
+    public String titulo;
+    public autor autor;
+    public Float preco;
+    public LocalDate dataLancamento;
+}
+
